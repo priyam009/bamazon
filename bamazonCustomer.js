07f20d8d0e69a4e151d5ad35e3b0ee1ec2d9e83f
Priyam <priyam009@live.com>
2019-09-04 00:25:02 +1000

    create inquirer prompt to get the id and qty from user and log the corresponding id value in the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
-var inquirer = require('inquirer');
-var mysql = require('mysql');
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
+//CREATING CONNECTION
 var connection = mysql.createConnection({
   host: "localhost",
   post: 3306,
@@ -9,22 +10,61 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+//CHECKING CONNECTION
 connection.connect(function(err) {
-  if(err) throw err;
+  if (err) throw err;
 
   console.log("connected as id " + connection.threadId + "\n");
 
-  readProducts();
+  // readProducts();
+  start();
 
-  connection.end();
-})
+});
 
-function readProducts() {
-  console.log("Selecting all products.... /n");
-
-  connection.query("SELECT * from products", function (err, res) {
-    if(err) throw err;
-
+//READS DATA IN THE DATABASE
+function readProducts(response) {
+  console.log("Selecting all products.... \n");
+  
+  var query = "SELECT * FROM products WHERE ?"
+  
+  connection.query(query, {item_id: response.id} ,function(err, res) {
+    if (err) throw err;
+    
     console.table(res);
+    connection.end();
+  });
+}
+
+//INQUIRER ASKS FOR INFORMATION- ID AND QTY FROM THE USER
+function start() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the id of the product to buy: ",
+      validate: function(value) {
+        if (!isNaN(value)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "Enter the quantity of the product to buy: ",
+      validate: function(value) {
+        if (!isNaN(value)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  ])
+  .then(function(response) {
+    console.log(response);
+    readProducts(response);
   })
 }
