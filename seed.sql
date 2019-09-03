e9825dc16f4b43f77c56ddeb94f700b15f13aa61
Priyam <priyam009@live.com>
2019-09-03 23:44:19 +1000

    create sql schema to create database bamazon with table products and columns and add mock up data rows using seeds file

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("iphone X", "electronics", 1000, 8),
+VALUES ("becoming", "books", 20, 50),
+VALUES ("pokemon: lets go", "games", 58, 30),
+VALUES ("nespresso coffee machine", "kitchen", 109, 10),
+VALUES ("nutribullet", "kitchen", 139, 32),
+VALUES ("chopping axe", "DIY", 22, 200),
+VALUES ("a dog's way home", "movies", 25, 33),
+VALUES ("apple airpods 2", "electronics", 276, 60),
+VALUES ("nintendo switch", "electronics", 460, 99),
+VALUES ("Eloquent Javascript", "books", 38, 489); 
