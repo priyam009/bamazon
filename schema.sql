707fc83f5197a1398b0635b4c8a77a3401aa2c17
Priyam <priyam009@live.com>
2019-09-06 23:23:56 +1000

    add new column product_sales to table products and create new table departments

--- schema.sql
+++ schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE products (
 
 SELECT *
 FROM products;
+
+ALTER TABLE products
+ADD product_sales INT NULL;
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NULL,
+  over_head_costs INT NULL,
+  PRIMARY KEY(department_id)
+)
